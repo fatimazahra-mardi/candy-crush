bc1aefea7d124d093385427efcde22e7d0b60af3
Kardady Salma <salma.kardady24@gmail.com>
2025-12-26 16:13:00 +0100

    Version finale

--- src/main/java/game/CandyCrushApp.java
+++ src/main/java/game/CandyCrushApp.java
@@ -1,10 +1,32 @@
 package game;
-
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
+import javafx.application.Platform;
+import javafx.scene.Node;
+import javafx.animation.PauseTransition;
+import javafx.scene.Cursor;
+
+
+
 
 public class CandyCrushApp extends Application {
 
@@ -14,15 +36,192 @@ public class CandyCrushApp extends Application {
         GridPane grid = GridView.createGrid(8, 8, 100); // taille augmentée
 
         BorderPane root = new BorderPane();
+        StackPane rootStack = new StackPane(); 
+        // 🔗 connexion avec GridView
+        GridView.setRootStack(rootStack);
+        
+            BackgroundImage bg = new BackgroundImage(
+            new Image(CandyCrushApp.class.getResourceAsStream("/image/image9.jpg")),
+            BackgroundRepeat.NO_REPEAT,
+            BackgroundRepeat.NO_REPEAT,
+            BackgroundPosition.CENTER,
+            new BackgroundSize(
+                    BackgroundSize.AUTO,
+                    BackgroundSize.AUTO,
+                    false,
+                    false,
+                    true,
+                    true
+            )
+        );
 
+        root.setBackground(new Background(bg));
+
+
+        // placer le jeu à droite
         // placer le jeu à droite
         root.setRight(grid);
 
+        // === SCORE PANEL CENTRÉ ===
+        StackPane scoreWrapper = new StackPane();
+        scoreWrapper.setPrefWidth(250); // largeur de la zone gauche
+        scoreWrapper.setAlignment(Pos.CENTER);
+
+        scoreWrapper.getChildren().add(GridView.createScorePanel());
+
+        root.setLeft(scoreWrapper);
+        BorderPane.setMargin(scoreWrapper, new Insets(40, 20, 40, 40));
+
+
         // marge à droite et en haut
-        BorderPane.setMargin(grid, new javafx.geometry.Insets(40, 40, 40, 40));
+        BorderPane.setMargin(grid, new Insets(
+        80,  // TOP  ⬆️
+        40,  // RIGHT
+        80,  // BOTTOM ⬇️
+        40   // LEFT
+        ));
+
+        rootStack.getChildren().add(root);
+        root.setVisible(false); // on cache le jeu au départ
+
+        // ================= LOADING SCREEN =================
+
+        StackPane loadingOverlay = new StackPane();
+        Image splash = new Image(
+        CandyCrushApp.class.getResourceAsStream("/image/splash.jpeg"),
+        1100,   // largeur = taille de la scène
+        800,    // hauteur = taille de la scène
+        false,  // preserveRatio → false = couvre tout
+        true    // smooth = true (IMPORTANT)
+        );
+
+
+            BackgroundImage loadingBg = new BackgroundImage(
+        splash,
+        BackgroundRepeat.NO_REPEAT,
+        BackgroundRepeat.NO_REPEAT,
+        BackgroundPosition.CENTER,
+        new BackgroundSize(
+            100, 100,     // largeur / hauteur en %
+            true, true,   // units = %
+            false,        // preserve ratio OFF → cover total
+            true          // cover = TRUE ⭐⭐⭐
+        )
+        );
+
+
+
+        loadingOverlay.setBackground(new Background(loadingBg));
+
+
+
+        // barre de progression
+        ProgressBar progressBar = new ProgressBar(0);
+        progressBar.setPrefWidth(400);     // un peu plus courte (ajuste 400-460)
+        progressBar.setPrefHeight(25);     // plus épaisse
+        progressBar.setMinHeight(20);
+        progressBar.setMaxHeight(30);
+        progressBar.setStyle("""
+            -fx-accent: #ff5aa5;
+            -fx-control-inner-background: #ffe4f0;
+        """);
+
+        // pourcentage
+        Label percentLabel = new Label("0%");
+        percentLabel.setStyle(
+            "-fx-font-size: 18; -fx-font-weight: bold; -fx-text-fill: #c2185b;"
+        );
+
+        // bouton Play (caché au début)
+        Button playButton = new Button("Play Now");
+        playButton.setVisible(false);
+        playButton.setStyle("""
+            -fx-background-color: #ff4fa1;
+            -fx-text-fill: white;
+            -fx-font-size: 22;
+            -fx-font-weight: bold;
+            -fx-padding: 10 40;
+            -fx-background-radius: 30;
+        """);
+        playButton.setCursor(Cursor.HAND);
+
+
+        // layout
+        VBox loadingBox = new VBox(20, progressBar, percentLabel, playButton);
+        // ✅ ICI EXACTEMENT
+        loadingBox.setAlignment(Pos.BOTTOM_CENTER);
+        loadingBox.setPadding(new Insets(0, 0, 60, 0));
+
+        // ensuite seulement on l’ajoute
+        loadingOverlay.getChildren().add(loadingBox);
+        rootStack.getChildren().add(loadingOverlay);
+        Platform.runLater(() -> {
+        Node bar = progressBar.lookup(".bar");
+        Node track = progressBar.lookup(".track");
+
+    if (bar != null) {
+        bar.setStyle("""
+            -fx-background-insets: 0;
+            -fx-background-radius: 10;
+            -fx-padding: 0;
+            -fx-background-color: 
+                repeating-linear-gradient(
+                    45deg,
+                    #ff5aa5 0px,
+                    #ff5aa5 10px,
+                    #e91e63 10px,
+                    #e91e63 20px
+                );
+        """);
+    }
+
+    if (track != null) {
+        track.setStyle("""
+            -fx-background-insets: 0;
+            -fx-background-radius: 10;
+            -fx-background-color: #ffe4f0;
+        """);
+    }
+});
+
+
+
+        Timeline loadingTimeline = new Timeline();
+        final double[] progress = {0};
+
+        loadingTimeline.getKeyFrames().add(
+        new KeyFrame(Duration.millis(15), e -> {
+        progress[0] += 0.01;
+        if (progress[0] >= 1) {
+            progress[0] = 1;
+            loadingTimeline.stop();
+            percentLabel.setText("100%");
+            PauseTransition pause = new PauseTransition(Duration.seconds(2)); // 2 ou 3
+            pause.setOnFinished(ev -> {
+                progressBar.setVisible(false);
+                percentLabel.setVisible(false);
+                playButton.setVisible(true);
+            });
+            pause.play();
+
+        } else {
+            percentLabel.setText((int)(progress[0] * 100) + "%");
+        }
+        progressBar.setProgress(progress[0]);
+        })
+        );
+
+        loadingTimeline.setCycleCount(Timeline.INDEFINITE);
+        loadingTimeline.play();
+
+        playButton.setOnAction(e -> {
+        rootStack.getChildren().remove(loadingOverlay); //  retire le loading
+        root.setVisible(true);                           // affiche le jeu
+        });
+
 
         // la taille de la scene (jeux)
-        Scene scene = new Scene(root, 1100, 800);
+        Scene scene = new Scene(rootStack, 1100, 800);
 
         stage.setTitle("Candy Crush");
         stage.setScene(scene);
--- src/main/java/game/GridView.java
+++ src/main/java/game/GridView.java
@@ -3,13 +3,23 @@ import javafx.scene.media.AudioClip;
 import java.net.URL;
 import javafx.animation.FadeTransition;
 import javafx.animation.PauseTransition;
+import javafx.animation.ScaleTransition;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.paint.Color;
 import javafx.util.Duration;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.Cursor;
 
 import java.util.Random;
 
@@ -20,9 +30,29 @@ public class GridView {
 
     private static StackPane firstSelected = null;
 
+    private static boolean gameOver = false;
+
     private static int ROWS;
     private static int COLS;
     private static int CELL_SIZE;
+    // ================== SCORE ==================
+    private static int score = 0;
+    private static Label scoreLabel;
+    private static int comboMultiplier = 1; // pour la cascade (1, puis 2, puis 3...)
+    // ================== MOVES ==================
+    private static int moves = 20;      // nombre de coups autorisés
+    private static Label movesLabel;    // affichage des coups restants
+    // ================== TARGET ==================
+    private static final int TARGET_SCORE = 6000;
+    private static Label targetLabel;
+
+    //=======================
+    
+    private static StackPane rootStack;
+
+
+
+
 
     private static final Random random = new Random();
 
@@ -83,6 +113,13 @@ private static void play(AudioClip clip) {
         }
 
         refreshView();
+        // init score UI si pas encore créé
+        if (scoreLabel == null) {
+            scoreLabel = new Label();
+            scoreLabel.setFont(Font.font(28));
+        }
+        updateScoreLabel();
+
         return grid;
     }
 
@@ -98,6 +135,8 @@ private static void play(AudioClip clip) {
 
     // ================== CLICK ==================
   private static void handleClick(StackPane cell) {
+
+    if (gameOver || moves <= 0) return;
 
     if (firstSelected == null) {
         firstSelected = cell;
@@ -129,19 +168,28 @@ private static void play(AudioClip clip) {
         if (hasMatch) break;
     }
 
+    moves--;                 // 1️⃣ on consomme un move
+    updateMovesLabel();      // 2️⃣ on met à jour l’UI
+
     if (hasMatch) {
         // ✔️ Coup valide → afficher le swap
         refreshView();
         resolveWithAnimation();
+
+
     } else {
         // ❌ Coup invalide → annuler immédiatement
          play(SOUND_GAME_OVER);
         swap(firstSelected, cell);
+        comboMultiplier = 1;
         refreshView();
+        
     }
 
     unselectCell(firstSelected);
     firstSelected = null;
+    checkEndGame();
+
 }
 
     private static boolean areAdjacent(StackPane a, StackPane b) {
@@ -164,17 +212,35 @@ private static void play(AudioClip clip) {
         boolean[][] matches = CandyLogic.detectMatches(board);
         boolean found = false;
 
+        int removedCount = 0;
+
         for (int r = 0; r < ROWS; r++) {
             for (int c = 0; c < COLS; c++) {
                 if (matches[r][c]) {
                     found = true;
-                    animateRemove(r, c);
+                    removedCount++;          // 1 on compte les bonbons supprimés
+                    animateRemove(r, c);     // 2 on lance l’animation de suppression
                 }
             }
         }
 
+
         if (!found) return;
         play(SOUND_MATCH);
+
+        if (!found) {
+            comboMultiplier = 1; // plus de match → fin de cascade → reset combo
+            return;
+        }
+
+        play(SOUND_MATCH);
+
+        // SCORE : 10 points par bonbon * multiplicateur de combo
+        int base = 10;
+        score += removedCount * base * comboMultiplier;
+        comboMultiplier++;
+        updateScoreLabel();
+
 
 
         PauseTransition wait = new PauseTransition(Duration.millis(350));
@@ -231,6 +297,102 @@ private static void play(AudioClip clip) {
             }
         }
     }
+    
+    private static void updateScoreLabel() {
+    scoreLabel.setText(String.valueOf(score));
+        // 🎬 Animation Candy Crush (score)
+        ScaleTransition st = new ScaleTransition(Duration.millis(150), scoreLabel);
+        st.setFromX(1);
+        st.setFromY(1);
+        st.setToX(1.2);
+        st.setToY(1.2);
+        st.setAutoReverse(true);
+        st.setCycleCount(2);
+        st.play();
+    }
+
+    public static VBox createScorePanel() {
+
+        Label title = new Label("Candy Crush");
+        title.setFont(Font.font("Arial", 26));
+
+        scoreLabel = new Label("0");
+        scoreLabel.setFont(Font.font("Arial", 20));
+
+        movesLabel = new Label();
+        movesLabel.setFont(Font.font("Arial", 18));
+        updateMovesLabel();
+
+        targetLabel = new Label();
+        targetLabel.setFont(Font.font("Arial", 18));
+        updateTargetLabel();
+
+
+        VBox targetBox = createInfoBox("Target", targetLabel);
+        VBox scoreBox  = createInfoBox("Score", scoreLabel);
+        VBox movesBox  = createInfoBox("Moves", movesLabel);
+
+        VBox box = new VBox(20);
+        box.getChildren().addAll(title, targetBox, scoreBox, movesBox);
+        box.setAlignment(Pos.CENTER);
+
+        VBox wrapper = new VBox();
+        wrapper.setAlignment(Pos.CENTER);     // centre verticalement
+        wrapper.setPadding(new Insets(30, 0, 30, 0)); // marge haut / bas
+        wrapper.getChildren().add(box);
+
+
+
+
+        box.setStyle(
+            "-fx-background-color: #f6c1d1;" +
+            "-fx-border-color: #c85a7a;" +
+            "-fx-border-width: 5;" +
+            "-fx-background-radius: 25;" +
+            "-fx-border-radius: 25;" +
+            "-fx-padding: 25;"
+        );
+
+        box.setPrefWidth(220);
+        box.setAlignment(Pos.TOP_CENTER);
+
+        return wrapper;
+        
+    }
+
+    private static VBox createInfoBox(String titleText, Label valueLabel) {
+
+    Label title = new Label(titleText);
+    title.setStyle("""
+        -fx-font-size: 14;
+        -fx-text-fill: #880e4f;
+        -fx-font-weight: bold;
+    """);
+
+    valueLabel.setStyle("""
+        -fx-font-size: 22;
+        -fx-text-fill: #c2185b;
+        -fx-font-weight: bold;
+    """);
+
+    VBox box = new VBox(6, title, valueLabel);
+    box.setAlignment(Pos.CENTER);
+    box.setPadding(new Insets(12));
+    box.setPrefWidth(190);
+
+    box.setStyle("""
+        -fx-background-color: #ffd6e7;
+        -fx-background-radius: 16;
+        -fx-border-color: #c2185b;
+        -fx-border-width: 3;
+        -fx-border-radius: 16;
+    """);
+
+    return box;
+    }
+
+
+
 
     // ================== VIEW ==================
     private static void refreshView() {
@@ -274,6 +436,134 @@ private static int generateSafeCandy(int r, int c) {
     private static void unselectCell(StackPane c) {
         c.setStyle("-fx-background-color:#f5f5f5; -fx-border-color:#dddddd;");
     }
+
+    private static void updateMovesLabel() {
+    movesLabel.setText(String.valueOf(moves));
+    }
+
+   private static void updateTargetLabel() {
+    targetLabel.setText(String.valueOf(TARGET_SCORE));
+    }
+
+
+    private static void checkEndGame() {
+    if (gameOver) return;
+
+    if (score >= TARGET_SCORE) {
+        gameOver = true;
+        showEndPopup(true);
+    }
+    else if (moves <= 0) {
+        gameOver = true;
+        showEndPopup(false);
+    }
+
+    }
+
+    private static void showEndPopup(boolean win) {
+
+    StackPane overlay = new StackPane();
+    overlay.setStyle("-fx-background-color: rgba(0,0,0,0.6);");
+
+    VBox popup = new VBox(20);
+    popup.setAlignment(Pos.CENTER);
+    popup.setPadding(new Insets(30));
+    // 🔹 largeur et hauteur contrôlées
+    popup.setMaxWidth(360);
+    popup.setPrefHeight(420);      // 👈 hauteur fixe (clé !)
+    popup.setMaxHeight(420);
+    popup.setMinHeight(380);
+    StackPane.setMargin(popup, new Insets(60, 0, 60, 0));
+
+
+    popup.setStyle("""
+        -fx-background-color: #ffd6e7;
+        -fx-background-radius: 25;
+        -fx-border-color: #c2185b;
+        -fx-border-width: 4;
+        -fx-border-radius: 25;
+    """);
+
+    Label title = new Label(win ? " 🎉 You Win!" : " 😢 You Failed!");
+    title.setFont(Font.font("Arial", FontWeight.BOLD, 26));
+    title.setTextFill(Color.web("#b0003a"));
+
+    Label scoreTitle = new Label("Final Score");
+    scoreTitle.setFont(Font.font("Arial", FontWeight.SEMI_BOLD, 16));
+    scoreTitle.setTextFill(Color.web("#7a1c3d"));
+
+    Label movesLeftLabel = null;
+    if (moves > 0) {
+        movesLeftLabel = new Label("You crushed it 💥 with " + moves + " moves left!");
+        movesLeftLabel.setFont(
+            Font.font("Arial", FontWeight.SEMI_BOLD, 17)
+        );
+
+        movesLeftLabel.setTextFill(
+            Color.web("#c2185b") // rose Candy Crush
+        );
+
+    }
+
+    Label scoreValue = new Label(String.valueOf(score));
+    scoreValue.setFont(Font.font("Arial", FontWeight.EXTRA_BOLD, 36));
+    scoreValue.setTextFill(Color.web("#c2185b"));
+
+    Button retry = new Button("Retry");
+    retry.setStyle("""
+        -fx-background-color: #ff4f9a;
+        -fx-text-fill: white;
+        -fx-font-size: 16;
+        -fx-padding: 10 30;
+        -fx-background-radius: 20;
+    """);
+    retry.setCursor(Cursor.HAND);
+
+    retry.setOnAction(e -> {
+        resetGame();
+        ((Pane) overlay.getParent()).getChildren().remove(overlay);
+    });
+
+    popup.getChildren().add(title);
+    popup.getChildren().add(scoreTitle);
+    popup.getChildren().add(scoreValue);
+
+    if (movesLeftLabel != null) {
+        popup.getChildren().add(movesLeftLabel);
+    }
+
+    popup.getChildren().add(retry);
+
+    overlay.getChildren().add(popup);
+
+    rootStack.getChildren().add(overlay);
+    
+    }
+
+    private static void resetGame() {
+    score = 0;
+    moves = 20;
+    comboMultiplier = 1;
+    gameOver = false;
+
+    updateScoreLabel();
+    updateMovesLabel();
+
+    refillBoard();
+    refreshView();
+    }
+
+
+
+    public static void setRootStack(StackPane root) {
+    rootStack = root;
+
+    }
+
+
+
+
+
 }
 
 
